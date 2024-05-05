e429c03f6e3d611a0e7ddc49c5f3257622f9fe0e
hafizmy01 <apihtechsolution@gmail.com>
2024-05-05 18:03:23 +0800

    tambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <h1>Selamat Datang Semua</h1>
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi earum labore eaque, quidem repudiandae consequuntur, eveniet delectus necessitatibus, in placeat sunt voluptatum eligendi dignissimos quas voluptate consectetur accusantium temporibus animi.</p>
-    
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
